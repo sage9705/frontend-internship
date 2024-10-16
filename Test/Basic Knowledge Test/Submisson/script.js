a6b6bb6bb8b8b2f1c19dbc6b6a1efce3407edb7c
Vera-poms <verapomaa99@gmail.com>
2024-10-16 19:56:09 +0000

    unfinished complete

--- Test/Basic Knowledge Test/Submisson/script.js
+++ Test/Basic Knowledge Test/Submisson/script.js
@@ -1,6 +1,16 @@
 const filterName = document.querySelector("[data-filter]");
 const productName = document.querySelector("[data-products]");
+const colorDots = document.getElementsByClassName("coloDots")
+var nav = document.querySelector("#primary-navigation");
+var toggleButton = document.querySelector("#toggleButton");
+var validEmail = document.querySelector("#email");
+var form = document.querySelector("form");
+errorMessage = document.getElementById("re");
+var regularExp = /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/;
 
+
+let currentColor = "blue";
+showColor(currentColor);
 let product = [];
 let data;
 let slideIndex = 1;
@@ -30,6 +40,28 @@ function showSlides(n){
   dots[slideIndex-1].className += " active";
 }
 
+function changeColor(color){
+  showColor(currentColor = color);
+}
+
+function showColor(color){
+  let dotss = document.getElementsByClassName("dot");
+
+}
+function validation() {
+  if (!validEmail.value.match(regularExp)) {
+    errorMessage.innerHTML = "Valid email required";
+    return false;
+  }
+  errorMessage.innerHTML = "";
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  submit();
+});
+
+
 filterName.addEventListener("input", (e) => {
     const value = e.target.value.toLowerCase();
     product.forEach((product)=> {
@@ -39,6 +71,18 @@ filterName.addEventListener("input", (e) => {
             product.price.toString().includes(value);
         product.element.classList.toggle("hide", !isVisible);
     });
+});
+
+toggleButton.addEventListener("click", ()=>{
+  const visibility = nav.getAttribute("data-visible");
+  if (visibility === "false"){
+    nav.setAttribute("data-visible", true);
+    toggleButton.setAttribute("aria-expanded", true);
+  }else{
+    nav.setAttribute("data-visible", false);
+    toggleButton.setAttribute("aria-expanded", false);
+  }
+  console.log(visibility);
 });
 
 
