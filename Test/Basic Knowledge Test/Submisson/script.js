353b95214c21e960a5199559f2698c170a532962
Vera-poms <verapomaa99@gmail.com>
2024-10-16 00:22:57 +0000

    progress

---
+++ Test/Basic Knowledge Test/Submisson/script.js
@@ -0,0 +1,53 @@
+const filterName = document.querySelector("[data-filter]");
+const productName = document.querySelector("[data-products]");
+
+let product = [];
+let data;
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function pushSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+  let i;
+  let slides = document.getElementsByClassName("slideShow");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+filterName.addEventListener("input", (e) => {
+    const value = e.target.value.toLowerCase();
+    product.forEach((product)=> {
+        const isVisible = 
+            product.name.toLowerCase().includes(value) ||
+            product.category.toLowerCase().includes(value) ||
+            product.price.toString().includes(value);
+        product.element.classList.toggle("hide", !isVisible);
+    });
+});
+
+
+product = data.map((product) => {
+    const productss = productName.content.cloneNode(true).children[0];
+    const header = productss.getElementsByClassName("description");
+    const image = productss.getElementsByClassName("image");
+    header.textContent = product.name;
+    image.textContent = product.category;
+    productName.append(product);
+    return { name: product.name, category: product.image, element: products };
+});
